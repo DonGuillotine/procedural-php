ced334cf78d95a9500ebd4a5997a4f68849e5f15
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-17 12:06:32 +0100

    PHP Conitional Statements

---
+++ conditionsl_statements.php
@@ -0,0 +1,13 @@
+<?php
+    //Conditional Statements eg: if statement, if....else statement if elseif...else statement, switch statement
+
+    /*
+    Syntax for if statement
+    if(condition){
+        code to be executed if condition is true
+    }
+    */
+
+    $time = date()
+
+?>
