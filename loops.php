6965e897866699a9879d0a7ad49ca01b7592ab7e
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-17 12:47:14 +0100

    PHP While Loop

---
+++ loops.php
@@ -0,0 +1,25 @@
+<?php
+    /*
+        Loops execute the same block of code over and over as long as the condition is true
+        eg. while, do..while loop, for loop, foreach loop
+    */
+
+    // While Loop
+    $age = 1;
+
+    while($age <= 5){
+        echo "The number is: $age <br>";
+        $age++;
+    }
+
+    $age_two = 0;
+
+    while($age_two <= 100){
+        echo "The number is: $age_two <br>";
+        $age_two+=10;
+    }
+
+
+    // Write a while loop that prints from 1 to 51
+
+?>
