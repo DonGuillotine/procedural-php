f89f0fadf8be717bbbdf59dcca0ef99c626378ce
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-24 13:19:15 +0100

    PHP do...while loops

--- loops.php
+++ loops.php
@@ -5,21 +5,39 @@
     */
 
     // While Loop
-    $age = 1;
 
-    while($age <= 5){
-        echo "The number is: $age <br>";
-        $age++;
-    }
+    // $age = 1;
 
-    $age_two = 0;
-
-    while($age_two <= 100){
-        echo "The number is: $age_two <br>";
-        $age_two+=10;
-    }
+    // while($age <= 5){
+    //     echo "Your age is " . $age;
+    //     echo "<br>";
+    //     $age++;
+    // }
 
 
-    // Write a while loop that prints from 1 to 51
+    // $age_two = 0;
 
+    // while($age_two <= 100){
+    //     echo "The number is: $age_two <br>";
+    //     $age_two = $age_two + 10;
+    // }
+
+    /* do...while
+        * do...while loop will execute a block of code once
+        * It will check the condition
+        * repeat the loop while the condition is true
+
+
+        do{
+            code to be executed
+        } while(condition is true)
+    */
+
+
+    $money = 1;
+
+    do{
+        echo "I have $money naira <br>";
+        $money++;
+    } while($money <= 5);
 ?>
