1c8f38fbc163e88dd7bcd6e42e6d8ba4ce340b8d
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-28 12:41:34 +0100

    Coding Challange Done

---
+++ coding_challange.php
@@ -0,0 +1,39 @@
+<?php
+// Define valid login credentials
+$valid_username = 'myusername';
+$valid_password = 'mypassword';
+
+// Check if form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	// Get input values from form
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	// Check if input values match valid credentials
+    // === is known as the identical operato
+	if ($username === $valid_username && $password === $valid_password) {
+		// Login successful
+		$message = 'Login successful!';
+		$success = true;
+	} else {
+		// Login failed
+		$message = 'Invalid username or password';
+		$success = false;
+	}
+} else {
+	// Form not submitted yet
+	$message = '';
+	$success = null;
+}
+
+// If login failed, keep asking for login details until login is successful
+while (!$success) {
+	include('login_form.php');
+    echo $message.'<br>';
+	exit();
+}
+
+// If login was successful, display success message
+echo $message.'<br>';
+echo $success ? 'Welcome, '.$valid_username.'!' : '';
+?>
---
+++ login_form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Form</title>
+</head>
+<body>
+	<h2>Login</h2>
+	<form action="login.php" method="post">
+		<label>Username:</label>
+		<input type="text" name="username"><br><br>
+		<label>Password:</label>
+		<input type="password" name="password"><br><br>
+		<input type="submit" value="Login">
+	</form>
+</body>
+</html>
