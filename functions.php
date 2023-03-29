220e41ed390f2ceca3804bce63d41f4eb20c23e2
Donald Nwokoro <infect3dlab@gmail.com>
2023-03-29 10:35:57 +0100

    PHP Functions

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+    // A function is a block of statements that can be used repeatedly in a program
+
+    // Will not execute when a page loads
+
+    // Will be executed by a call to the function ie the function must be invoked or called
+
+
+    function writeMessage(){
+        echo "Hello World";
+    }
+
+    writeMessage();
+?>
