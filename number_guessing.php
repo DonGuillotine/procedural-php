94ea1232acdf011c5eac0064f6f8cc283e5752d9
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-03 20:23:37 +0100

    Created validateUserInput Function

--- number_guessing.php
+++ number_guessing.php
@@ -3,4 +3,8 @@
         // Return Random Numbers from 1 - 100
         return rand(1, 100);
     }
+
+    function validateUserInput($input) {
+        return is_numeric($input) && $input >= 1 && $input <= 100;
+    }
 ?>
