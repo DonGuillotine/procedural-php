95747b57451c20db370121b2734bf24082befabd
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-03 20:25:15 +0100

    Created provideFeedback Function

--- number_guessing.php
+++ number_guessing.php
@@ -8,4 +8,14 @@
         // Validating user input so only numbers ranging from 1 - 100 are entered
         return is_numeric($input) && $input >= 1 && $input <= 100;
     }
+
+    function provideFeedback($guess, $number) {
+        if ($guess < $number) {
+            echo "Too low!";
+        } elseif ($guess > $number) {
+            echo "Too high!";
+        } else {
+            echo "Congratulations! You guessed the number.";
+        }
+    }
 ?>
