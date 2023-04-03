6d5353017c430f00315a070ce617876776803f97
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-03 20:24:27 +0100

    Created validateUserInput Function Comment

--- number_guessing.php
+++ number_guessing.php
@@ -5,6 +5,7 @@
     }
 
     function validateUserInput($input) {
+        // Validating user input so only numbers ranging from 1 - 100 are entered
         return is_numeric($input) && $input >= 1 && $input <= 100;
     }
 ?>
