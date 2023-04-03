b7f4d6b04f4fab7c3841db47358295a81a49dfae
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-03 20:59:54 +0100

    Changed Code Logic Fixed

--- number_guessing.php
+++ number_guessing.php
@@ -50,7 +50,7 @@
 		<input type="number" name="guess">
         <input type="submit" name="submit" value="Submit">
         <?php if (!empty($feedback)) { ?>
-            <p><?php echo $feedback; ?></p>
+            <p><?php echo $feedback . $number; ?></p>
         <?php } ?>
     </form>
 </body>
