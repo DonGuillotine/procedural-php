53cd4678816a43609aa0e646d3ecab8c89f18df0
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-04 11:49:01 +0100

    PHP Sorting Arrays

--- arrays.php
+++ arrays.php
@@ -79,4 +79,14 @@
             }
         echo "</ul>";
     }
+
+
+    /* Sorting Arrays
+        sort() - sort arrays in ascending order
+        rsort() - sort arrays in descending order
+        asort() - sort associative arrays in ascending order, according to the value
+        ksort() - sort associative arrays in ascending order, according to the key
+        arsort() - sort associative arrays in descending order, according to the value
+        krsort() - sort associative arrays in descending order, according to the key
+    */
 ?>
