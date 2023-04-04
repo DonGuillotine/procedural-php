70ec66c54b46880b3045ae0b302fd9b858470f6e
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-04 12:03:01 +0100

    PHP Super Globals

---
+++ superglobals.php
@@ -0,0 +1,15 @@
+/*
+
+    PHP Super Globals are predefined varaibles in PHP
+    They are always accesible regardless of the scope
+
+    $GLOBALS - It is used to access global variables from anywhere
+    $_SERVER - It holds info about headers, paths and script locations
+    $_REQUEST - It is used to collect data after submitting a HTML form
+    $_POST - It is used to collect form data after submitting a HTML form with method=post
+    $_GET- It is used to collect data after submitting a HTML form with method=get
+    $_FILES - Mainly used for submitting media_> images, pdf, video etc multipart/form-data
+    $_ENV - It stores environment variables available to current script
+    $_COOKIE - It stores cookies
+    $_SESSION - for session stuff
+*/
