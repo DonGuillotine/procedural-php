d26b09c8684da258b3ca9ef99cb13f584cd28693
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-05 11:53:31 +0100

    PHP Regular Expressions II

--- regular_expressions.php
+++ regular_expressions.php
@@ -3,4 +3,18 @@
     // "/" is the delimiter and "i" is a modifier that makes the search case-insensitive
     $pattern = "/world/i";
     echo preg_match($pattern, $str);
+
+    echo "<br>";
+
+    $sentence = "The rain in Spain falls mainly on the plains of Gucci Maine";
+    $pattern = "/ain/i";
+    echo preg_match_all($pattern, $sentence);
+
+    /*
+        Regular Expression Functions -> preg_replace()
+        i - case-insensitive search
+        m - multiline search
+        u - enables correct matching of UTF-8 encoded patterns
+
+    */
 ?>
